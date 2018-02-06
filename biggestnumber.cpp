4db38c7240762caf8dd7d6dd18bd3ee888ac1671
jackzajac7 <azajac1@mail.csuchico.edu>
2018-02-06 11:24:59 -0800

    comparison of values if's

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -1,17 +1,36 @@
-//Author:
+//Author: Aero Zajac
 #include<iostream>
 
 using namespace std; //so we don't need the prefix std::
 
 int main()
 {
-  
-  cout<<"Please enter a whole number:\n";
-  
-  cout<<"Please enter another whole number:\n";
 
-  cout<<"Of those two numbers, the biggest is: ";
-  cout<<endl<<"Thank you for playing.\n";
+        int num1=0;
+        int num2=0;
+        int biggest=0;
 
-  return 0;
+
+        cout<<"Please enter a whole number:\n";
+
+        cin>>num1;
+
+        cout<<"Please enter another whole number:\n";
+
+        cin>>num2;
+
+        if(num1>num2) {
+                biggest = num1;
+        }
+        else if(num2>num1) {
+                biggest = num2;
+        }
+        else {
+                cout<<"You entered in the same value or non whole numbers.";
+        }
+
+        cout<<"Of those two numbers, the biggest is: "<<biggest<<endl;
+        cout<<endl<<"Thank you for playing.\n";
+
+        return 0;
 }
